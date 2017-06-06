820d39621a32b7ebd0d347860903d16655647f88
dasHerbsti <carinaherbst@gmx.de>
2017-06-06 13:35:36 +0200

    add dbConfig.php

---
+++ dbConfiguration.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '');
+
+define('MYSQL_DATABASE','shoppinglist');
+
+
+?>
