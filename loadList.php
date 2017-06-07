1d1268e3236cccc06856b560d51b5ad8a7f36e11
dasHerbsti <carinaherbst@gmx.de>
2017-06-07 20:29:55 +0200

    add icons plus/minuse/remove to shopping list

--- loadList.php
+++ loadList.php
@@ -1,36 +1,41 @@
 <?php
-			require_once('dbConfiguration.php');
+	require_once('dbConfiguration.php');
 
-			$db_link=mysqli_connect(
-					MYSQL_HOST,
-					MYSQL_USER,
-					MYSQL_PASSWORD,
-					MYSQL_DATABASE
-					);
-					
-		if (!$db_link) 
-		{
-			die("Connection failed: " . mysqli_connect_error());
-		}
+	$db_link=mysqli_connect(
+			MYSQL_HOST,
+			MYSQL_USER,
+			MYSQL_PASSWORD,
+			MYSQL_DATABASE
+			);
+			
+	if (!$db_link) 
+	{
+		die("Connection failed: " . mysqli_connect_error());
+	}
 
-		$sql = "SELECT Product, Category, Number FROM listentries WHERE UserID=1";
-		$result = mysqli_query($db_link, $sql);
+	$sql = "SELECT Product, Category, Number FROM listentries WHERE UserID=1";
+	$result = mysqli_query($db_link, $sql);
 
-		if($result != FALSE)
+	if($result != FALSE)
+	{
+		while($row = mysqli_fetch_assoc($result)) 
 		{
-			while($row = mysqli_fetch_assoc($result)) 
-			{
-				?>
-					<li class="listelement">
-						<div class="listtext"><?php echo $row['Number']?></div>
-						<div class="listtext"><?php echo $row['Product']?></div>
-					</li>
-				<?php
-			}
+			?>
+				<li class="listelement">
+					<div class="listtext"><?php echo $row['Number']?></div>
+					<div class="listtext"><?php echo $row['Product']?></div>
+					<div style="float:right">
+						<a href="removeElement.js"><i class="material-icons md-24">&#xE928;</i></a>
+						<a href="numberPlus.js"><i class="material-icons md-24">&#xE145;</i></a>
+						<a href="numberMinus.js"><i class="material-icons md-24">&#xE15B;</i></a>
+					</div>
+				</li>
+			<?php
 		}
-		else
-		{
-			echo "Query failed";
-		}
-		mysqli_close($db_link);
-		?>
+	}
+	else
+	{
+		echo "Query failed";
+	}
+	mysqli_close($db_link);
+?>
