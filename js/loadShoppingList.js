d0c798a71ce513107bc167e661420b8c80395f8a
dasHerbsti <carinaherbst@gmx.de>
2017-06-14 15:04:14 +0200

    load categories from database AJAX

--- js/loadShoppingList.js
+++ js/loadShoppingList.js
@@ -1,11 +1,12 @@
-loadDoc("loadList.php");
+loadDoc("loadShoppingList.php", "list");
+loadDoc("loadCategoryList.php", "categories");
 
-function loadDoc(phpSource) {
+function loadDoc(phpSource, id) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 		console.log(typeof this.responseText);		
-    document.getElementById("list").innerHTML+=this.responseText;
+    document.getElementById(id).innerHTML+=this.responseText;
 	}	
   };
   xhttp.open("GET", phpSource, true);
---
+++ loadCategoryList.php
@@ -0,0 +1,35 @@
+<?php
+	require_once('dbConfiguration.php');
+
+	$db_link=mysqli_connect(
+			MYSQL_HOST,
+			MYSQL_USER,
+			MYSQL_PASSWORD,
+			MYSQL_DATABASE
+			);
+			
+	if (!$db_link) 
+	{
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
+	$sql = "SELECT name FROM categories INNER JOIN positions ON positions.CategoryId=categories.id WHERE positions.UserId=1 ORDER BY positions.Position";
+	$result = mysqli_query($db_link, $sql);
+
+	if($result != FALSE)
+	{
+		while($row = mysqli_fetch_assoc($result)) 
+		{
+			?>
+				<li>
+					<a><?php echo $row['name']?></a>
+				</li>
+			<?php
+		}
+	}
+	else
+	{
+		echo "Query failed";
+	}
+	mysqli_close($db_link);
+?>
