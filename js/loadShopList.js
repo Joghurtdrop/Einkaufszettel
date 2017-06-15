36587c62411924f468355438e85d28a06b2ed7a8
Unknown <d.diewold@gmx.net>
2017-06-15 13:46:26 +0200

    get category from server

---
+++ js/loadShopList.js
@@ -0,0 +1,19 @@
+loadDoc("loadShopCategoryList.php", "verticallist");
+
+function loadDoc(phpSource, id) 
+{
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+		//console.log(typeof this.responseText);		
+    document.getElementById(id).innerHTML=this.responseText;
+	}	
+  };
+  xhttp.open("GET", phpSource, true);
+  xhttp.send();
+}
+
+function setheight() {
+	
+}
+
