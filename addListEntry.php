f70134e8b58efa30a3d2a99fc2ffc18c87a80b64
dasHerbsti <carinaherbst@gmx.de>
2017-06-15 16:18:39 +0200

    add list entry working

--- addListEntry.php
+++ addListEntry.php
@@ -1,11 +1,10 @@
 <?php
-	/*$productName = $_POST("productName");
-	$productNumber=$_POST("productNumber");
-	$categoryId=$_POST("categoryId");*/
+
+	echo $_POST['productName'];	$productName = $_POST["productName"];
+	$productNumber=$_POST["productNumber"];
+	$categoryId=$_POST["categoryId"];
 	
 	include 'dataAccess.php';
 	
-	//addEntry(1,$productName, $productNumber, $categoryId);	
-	//echo getEntry(1,1);
-	addEntry(1,'Banane', 5, 2);
+	addEntry(1,$productName, $productNumber, $categoryId);	
 ?>
