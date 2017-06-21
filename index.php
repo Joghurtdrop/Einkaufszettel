c2dd50beb59a136129e045ef70fe04fad08ce152
dasHerbsti <carinaherbst@gmx.de>
2017-06-21 10:33:45 +0200

    remove logout profile

--- index.php
+++ index.php
@@ -17,12 +17,11 @@ include 'menubar.php';
 	<h1>Dein Konto</h1>
 	<?php echo $login_status;?>
 	<div class="row">
-		<div class="col-10 row">
+		<div class="col-12 row">
 			<label class="col-4">Füge einen neuen Markt hinzu:</label>
 			<input class="col-6" id="newShopName"/>
 			<div onClick="addShop()" class="button col-2"/>Okay</div>          
 		</div>      
-		<div class="col-2 buttonHolder"><a class="button" href="logout.php">Logout</a></div>
 	</div>
 	<h1>Dein ausgewählter Markt</h1>
 	<div id="listholder"></div>
